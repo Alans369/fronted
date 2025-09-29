f5b0ee19415101fb7b23686270fa791266699d77
Alan Edenilson Campos Sanchez <cs25001@esfe.agape.edu.sv>
2025-09-29 16:29:17 -0600

    enviando datos

--- noticiasApp/src/Register.tsx
+++ noticiasApp/src/Register.tsx
@@ -12,11 +12,7 @@ export function Register() {
         rolId:1
     }
 
-
        const[forData,setForData] = useState<Usuario>(data)
-
-
-       console.log(forData)
 
         const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
         const name = e.target.name;
@@ -24,21 +20,25 @@ export function Register() {
 
         if(name==="rolId"){
             setForData(values => ({...values, [name]: parseInt(value)}))
-            console.log(forData)
             return
         }
-       setForData(values => ({...values, [name]: value}))
-       
-     
-       
-    }
+         setForData(values => ({...values, [name]: value}))
+
+       }
+
+
+       async function handleSubmit(e: React.FormEvent) {
+                e.preventDefault();
+                alert('enviando datos');
+                console.log(forData)
+            }
 
 
 
 
     return (
         <div className=" flex p-20">
-            <form className="max-w-4xl mx-auto p-4">
+            <form  onSubmit={handleSubmit} className="max-w-4xl mx-auto p-4">
           
                 <div className="grid sm:grid-cols-2 gap-6">
                     <div className="relative flex items-center">
@@ -136,7 +136,7 @@ export function Register() {
                              
                              name="rolId"
                              value="1" 
-                             checked={forData.rolId.toString() === "1"} 
+                             checked={forData.rolId === 1} 
                              onChange={handleChange}
                              className="w-5 h-5" />
                             <label className="text-sm text-black ml-4">Administardor</label>
@@ -147,7 +147,7 @@ export function Register() {
                             onChange={handleChange}
                              name="rolId"
                              value="2" 
-                             checked={forData.rolId.toString() === "2"}  className="w-5 h-5"  />
+                             checked={forData.rolId === 2}  className="w-5 h-5"  />
                             <label className="text-sm text-black ml-4">Cliente</label>
                         </div>
 
@@ -155,7 +155,7 @@ export function Register() {
                     </div>
                 </div>
 
-                <button type="button"
+                <button type="submit"
                     className="mt-12 px-4 py-2 w-full text-[15px] font-medium bg-black text-white hover:bg-[#222] rounded-sm cursor-pointer">Submit</button>
             </form>
         </div>
