c25998f6d573611eb9a75a87096328778acfa41e
Alan Campos <cs25001@esfe.agape.edu.sv>
2025-09-30 17:26:58 +0000

    22

--- noticiasApp/src/Login.tsx
+++ noticiasApp/src/Login.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AuthService } from "./services/Auth";
 import Navbar from "./Navbar";
 import { useNavigate } from "react-router-dom";
@@ -12,6 +12,8 @@ const Login = () => {
   //const request=new AuthService()
   const [inputs, setInputs] = useState({ login: '', clave: '' })
   const [see,setSee] = useState(false)
+
+   const [isVisible,setIsVisible] = useState(false)
    
 
   console.log('render');
@@ -24,6 +26,7 @@ const Login = () => {
      
     } catch (err) {
       console.error('Login falló', err);
+      setIsVisible(true)
     }
     
   }
@@ -38,6 +41,19 @@ const Login = () => {
   function handleSee(){
     setSee(!see)
   }
+
+    useEffect(() => {
+                          // Si el mensaje es visible, inicia un temporizador
+                          if (isVisible) {
+                            const timer = setTimeout(() => {
+                              setIsVisible(false);
+                             
+                            }, 2000);
+                      
+                            // Limpia el temporizador si el componente se desmonta antes de que pase el segundo
+                            return () => clearTimeout(timer);
+                          }
+                        }, [isVisible]);
 
 
   return (
@@ -94,6 +110,36 @@ const Login = () => {
                 </div>
                 <p className="text-slate-900 text-sm !mt-6 text-center">Don't have an account? <span onClick={()=>{navigate('/register')}} className="text-blue-600 hover:underline ml-1 whitespace-nowrap font-semibold">Register here</span></p>
               </form>
+
+
+                { isVisible && <div className="bg-red-50 text-red-800 p-6 rounded-lg relative" role="alert">
+              <div className="mb-3 flex items-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="w-[18px] cursor-pointer fill-red-500 inline mr-3 shrink-0" viewBox="0 0 32 32">
+                      <path
+                          d="M16 1a15 15 0 1 0 15 15A15 15 0 0 0 16 1zm6.36 20L21 22.36l-5-4.95-4.95 4.95L9.64 21l4.95-5-4.95-4.95 1.41-1.41L16 14.59l5-4.95 1.41 1.41-5 4.95z"
+                          data-original="#ea2d3f" />
+                  </svg>
+                  <p className="font-semibold text-[15px] mr-3">Error Message!</p>
+              </div>
+
+              <span className="block sm:inline text-sm font-medium">.credendenciales invalidas</span>
+
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-3 h-3 cursor-pointer fill-red-500 absolute right-4 top-4"
+                  viewBox="0 0 320.591 320.591">
+                  <path
+                      d="M30.391 318.583a30.37 30.37 0 0 1-21.56-7.288c-11.774-11.844-11.774-30.973 0-42.817L266.643 10.665c12.246-11.459 31.462-10.822 42.921 1.424 10.362 11.074 10.966 28.095 1.414 39.875L51.647 311.295a30.366 30.366 0 0 1-21.256 7.288z"
+                      data-original="#000000" />
+                  <path
+                      d="M287.9 318.583a30.37 30.37 0 0 1-21.257-8.806L8.83 51.963C-2.078 39.225-.595 20.055 12.143 9.146c11.369-9.736 28.136-9.736 39.504 0l259.331 257.813c12.243 11.462 12.876 30.679 1.414 42.922-.456.487-.927.958-1.414 1.414a30.368 30.368 0 0 1-23.078 7.288z"
+                      data-original="#000000" />
+              </svg>
+
+              <a href="javascript:void(0)" className="border-b border-red-800 block w-max text-sm font-medium text-red-800 mt-3">Learn more</a>
+          </div> } 
+
+              
+
+
             </div>
           </div>
         </div>
